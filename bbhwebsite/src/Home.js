e63b10599bf15061f45f7b6788c93449a0448fe6
Leonardo Navarro <adriano_navarro@outlook.com>
2024-03-31 21:24:28 -0300

    Fixed API error

--- bbhwebsite/src/Home.js
+++ bbhwebsite/src/Home.js
@@ -12,7 +12,7 @@ function Home() {
     useEffect(() => {
       const fetchContestants = async () => {
         try {
-          const response = await fetch('http://193.31.31.105:25709/contestants');
+          const response = await fetch('https://193.31.31.105:25709/contestants');
           if (!response.ok) {
             console.log(response)
             throw new Error('Failed to fetch contestants');
